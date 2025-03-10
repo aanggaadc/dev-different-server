791823febbed2fe5075a2cf700abbc2ad5f5cb5b
anggara setiawan <aanggaadc@gmail.com>
2025-03-10 23:32:26 +0700

    create models Image

---
+++ src/models/Image.ts
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+const ImageSchema = new mongoose.Schema({
+  filename: { type: String, required: true },
+  path: { type: String, required: true },
+  uploadDate: { type: Date, default: Date.now },
+  size: { type: Number, required: true },
+  mimetype: { type: String, required: true },
+});
+
+export default mongoose.model("Image", ImageSchema);
