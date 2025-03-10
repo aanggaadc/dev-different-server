7230e0b824282028a7412011716b474dda8e17eb
anggara setiawan <aanggaadc@gmail.com>
2025-03-10 23:18:06 +0700

    create middleware upload

---
+++ src/middlewares/upload.ts
@@ -0,0 +1,27 @@
+import multer from "multer";
+// import path from "path";
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "uploads/");
+  },
+  filename: (req, file, cb) => {
+    cb(null, `${Date.now()}-${file.originalname}`);
+  },
+});
+
+const fileFilter = (
+  req: Express.Request,
+  file: Express.Multer.File,
+  cb: multer.FileFilterCallback
+) => {
+  if (file.mimetype.startsWith("image/")) {
+    cb(null, true);
+  } else {
+    cb(new Error("Only images are allowed"));
+  }
+};
+
+const upload = multer({ storage, fileFilter });
+
+export default upload;
